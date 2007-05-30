895a8cd1182d7c302995c6b5c512beefc31d4bc8
adparker <adparker@7a45735d-0e30-0410-86b5-c7a60505f560>
2007-05-30 23:25:53 +0000

    saving work

--- getdata.py
+++ getdata.py
@@ -11,37 +11,39 @@ from SOAPpy import WSDL
 #for cmdInput in sys.argv:
 #       print cmdInput
 
-def getstuff(user_name, iteration=0, length=500):
+def getstuff(table_name, iteration=0, length=500):
     soapClient = SOAPpy.SOAPProxy("http://sensorbase.org/soap/dataGet.php")
-    value = soapClient.getData('august@cs.ucla.edu', 'audio', \
-                               "image,time_stamp,user_name", \
-                               "p_48_Images", \
-                               "1 AND user_name='%s' ORDER BY time_stamp ASC" % user_name, \
-                               iteration * length, length, "csv")
-    rows = value.split('\n')
-    allrows = []
-    for row in rows:
-        if len(row) == 0:
-            continue
-        s = row.split(',')
-        allrows.append(s)
-    count = 1
-    length = len(allrows)
-    for row in allrows:
-        #    commands.getstatusoutput("wget 'http://www.sensorbase.org/%s' -O %04i.jpg" % (row[0], count))
-        print "[%03i/%05i/%05i] http://www.sensorbase.org/%s '%s/%s %s.jpg'" % (iteration, count, length, row[0], user_name, row[1], row[2])
-        commands.getstatusoutput("wget 'http://www.sensorbase.org/%s' -O '%s/%s %s'.jpg" % (row[0], user_name, row[1], row[2]))
-        count += 1
-    return length
+    value = soapClient.getData('adparker@gmail.com', 'ecopda', \
+                               "file_size_bytes,response_reason,response_status,signal_bars,signal_dbm,time_data,time_download,time_epoch", \
+                               "p_153_%s"%table_name, \
+                               "ORDER BY time_epoch ASC", \
+                               iteration * length, length,
+                               "csv")
+    print rows
+    # rows = value.split('\n')
+#     allrows = []
+#     for row in rows:
+#         if len(row) == 0:
+#             continue
+#         s = row.split(',')
+#         allrows.append(s)
+#     count = 1
+#     length = len(allrows)
+#     for row in allrows:
+#         #    commands.getstatusoutput("wget 'http://www.sensorbase.org/%s' -O %04i.jpg" % (row[0], count))
+#         print "[%03i/%05i/%05i] http://www.sensorbase.org/%s '%s/%s %s.jpg'" % (iteration, count, length, row[0], user_name, row[1], row[2])
+#         commands.getstatusoutput("wget 'http://www.sensorbase.org/%s' -O '%s/%s %s'.jpg" % (row[0], user_name, row[1], row[2]))
+#         count += 1
+#     return length
 
 
 
 def main():
     iteration = 0
-    user_name = sys.argv[1]
-    print "user_name is: " + user_name
+    table_name = sys.argv[1]
+    print "table_name is: " + user_name
     while(True):
-        if (getstuff(user_name, iteration) == 0):
+        if (getstuff(table_name, iteration) == 0):
             break
         iteration += 1
 
