ac7f3d428e99682608fce33c3ca659d5008f9f5b
adparker <adparker@7a45735d-0e30-0410-86b5-c7a60505f560>
2007-06-02 00:16:12 +0000

    saving work.

--- report.r
+++ report.r
@@ -1,25 +1,63 @@
+setwd("C:/cygwin/home/adparker/repositories/gprstest/trunk")
+source("report_util.r")
 setwd("C:/cygwin/home/adparker/repositories/gprstest/trunk/data")
-
 ## Read in a bunch of data
-df <- get.table("ahome.txt") # Andrew Home GPRS
-ahome1 <- split.table(df, 13643.45, 13646.42)
-ahome2 <- split.table(df, 13658.62, 13664.36)
-alaptop <- split.table(get.table("alaptop.txt")) ## Andrew Home DSL 
-censlab <- split.table(get.table("censlab.txt"))  ## Andrew's Desk
-## CENS Lab 5/15 static
-## two hours in late afternoon under static and optimal conditions
-censlab515 <- split.table(get.table("censlab_5_15_static.txt"))
-censlab516 <- split.table(get.table("censlab_5_16.txt"))  # CENS Lab 5/16
-censlab522 <- split.table(get.table("censlab_5_22.txt")) ## CENS Lab 5/22
-censlabwifi <- split.table(get.table("censlabwifi.txt")) ## CENS Lab WIFI
-chome <- split.table(get.table("chris_home_gprs.txt")) ## Chris Home GPRS
-lablaptopgrps <- split.table(get.table("lab_laptop_gprs.txt")) # lab laptop gprs
-labwifi1k <- split.table(get.table("lab_wifi_test_1k.txt")) # wifi test 1k
-valley515 <- split.table(get.table("valleydata_5_15.txt")) # Valley 5/15
-valley520 <- split.table(get.table("valley_5_20.txt")) ## Valley 5/20
-ww516 <- split.table(get.table("ww_5_16.txt")) ## Westwood 5/16
-ww517 <- split.table(get.table("ww_5_17.txt"))## Westwood 5/17
-ww518 <- split.table(get.table("ww_5_18.txt")) ## Westwood 5/18
 
-plot(ahome1$t1k$chron, ahome1$t1k$time_download)
+ahome1 <- get.split.table("ahome.txt", start.chron=13643.45, end.chron = 13646.42) # Andrew Home GPRS
+ahome2 <- get.split.table("ahome.txt", start.chron=13658.62, end.chron=13663.80)
+ahome3 <- get.split.table("ahome.txt",start.chron=13663.80, end.chron=Inf)
+alaptop <- get.split.table("alaptop.txt") ## Andrew Home DSL 
+censlab <- get.split.table("censlab.txt")  ## Andrew's Desk
+censlab515 <- get.split.table("censlab_5_15_static.txt") ## CENS 5/15
+#censlab516 <- get.split.table("censlab_5_16.txt")  # CENS Lab 5/16
+censlab522 <- get.split.table("censlab_5_22.txt") ## CENS Lab 5/22
+censlabwifi <- get.split.table("censlabwifi.txt") ## CENS Lab WIFI
+chome <- get.split.table("chris_home_gprs.txt") ## Chris Home GPRS
+lablaptopgrps <- get.split.table("lab_laptop_gprs.txt") # lab laptop gprs
+labwifi1k <- get.split.table("lab_wifi_test_1k.txt") # wifi test 1k
+valley515 <- get.split.table("valleydata_5_15.txt") # Valley 5/15
+valley520 <- get.split.table("valley_5_20.txt") ## Valley 5/20
+ww516 <- get.split.table("ww_5_16.txt") ## Westwood 5/16
+ww517 <- get.split.table("ww_5_17.txt")## Westwood 5/17
+ww518 <- get.split.table("ww_5_18.txt") ## Westwood 5/18
+
+
+t <- rbind.tables(ahome1, ahome2)
+t <- rbind.tables(t, ahome3)
+attach(t$k10)
+boxplot(time_download ~ weekday, ylim = c(0,30))
+boxplot(time_download ~ hour+weekday, ylim = c(0,30))
+boxplot(time_download ~ hour, ylim = c(0,30))
+boxplot(time_download ~ minute, ylim = c(0,30))
+
+foo <- lm(time_download ~ time_epoch)
+summary(foo)
+plot(foo)
+
+plot(time_download ~ time_epoch)
+abline(coef(foo),col="red",lw=5)
+
+detach(t$k10)
+## Plot stuff against signal strength (bars & dbm)
+boxplot(ahome1$k100$time_download ~ ahome1$k100$signal_dbm)
+boxplot(ahome1$k50$time_download ~ ahome1$k50$signal_dbm,add=T,col="red")
+boxplot(ahome1$k100$time_download ~ ahome1$k100$signal_bars)
+boxplot(ahome1$k50$time_download ~ ahome1$k50$signal_bars,add=T,col="red")
+
+boxplot(signal_bars ~ hour)
+boxplot(time_download/3 ~ hour, add=T,col="red")
+boxplot(signal_bars ~ hour)
+boxplot(time_download/2 ~ hour, add=T,col="red")
+
+## printing other stuff
+plot(ahome2$k1$chron, ahome2$k1$time_download, ylim=c(0,15))
+points(ahome2$k100$chron, ahome2$k100$time_download, col="red")
+points(ahome2$k10$chron, ahome2$k10$time_download, col="blue")
+points(ahome2$k50$chron, ahome2$k50$time_download, col="green")
+points(ahome2$k1$chron, ahome2$k1$signal_dbm - 80, col="orange")
+
+points(ahome3$k50$chron, ahome3$k50$time_download, col="purple")
+points(ahome3$k10$chron, ahome3$k10$time_download, col="orange")
+points(ahome3$k1$chron, ahome3$k1$time_download, col="brown")
+
 
--- report_util.r
+++ report_util.r
@@ -2,19 +2,51 @@ library(chron)
 
 get.table <- function(table.name) {
   table <- read.table(table.name,header=TRUE,sep=",")
-  date.and.time = matrix(unlist(strsplit(as.character(table$time_date)," ")), ncol = 2, byrow=TRUE)
-  chrons = chron(date.and.time[,1], date.and.time[,2],  format=c(dates="y-m-d", times="h:m:s"))
-  table <- transform(table, chron=chrons)
+  date.and.time <-
+    matrix(unlist(strsplit(as.character(table$time_date)," ")),
+           ncol = 2, byrow=TRUE)
+  chrons <- chron(date.and.time[,1],
+                  date.and.time[,2],
+                  format=c(dates="y-m-d", times="h:m:s"))
+  days <- cut(chrons,"day")
+  weekday <- weekdays(chrons)
+  hour <- hours(chrons)
+  minute <- minutes(chrons)
+  second <- seconds(chrons)
+  
+  table <- transform(table,
+                     chron=chrons,
+                     day=days,
+                     weekday = weekday,
+                     hour = hour,
+                     minute = minute,
+                     second = second)
   return(table)
 }
 
-split.table <- function(t, start.chron = 0, end.chron = Inf) {
-  t <- t[t$chron >= start.chron && t$chron <= end.chron]
-  t1k <- t[t$file_size_bytes == 10240,]
-  t10k <- t[t$file_size_bytes == 10240,]
-  t50k <- t[t$file_size_bytes == 51200,]
-  t100k <- t[t$file_size_bytes == 102400,]
-  return(list(t1k=t1k,t10k=t10k,t50k=t50k,t100k=t100k))
+split.table <- function(table, start.chron = 0, end.chron = Inf) {
+  t <- table[start.chron <= table$chron & table$chron < end.chron,]
+  k1 <- t[t$file_size_bytes == 1024,]
+  k10 <- t[t$file_size_bytes == 10240,]
+  k50 <- t[t$file_size_bytes == 51200,]
+  k100 <- t[t$file_size_bytes == 102400,]
+  return(list(k1=k1,k10=k10,k50=k50,k100=k100))
 
 }
 
+get.split.table <- function(table.name,
+                            start.chron = 0,
+                            end.chron = Inf) {
+  t <- get.table(table.name)
+  tt <- split.table(t, start.chron = start.chron, end.chron = end.chron)
+  return (tt)
+}
+
+rbind.tables <- function(t1, t2) {
+  
+  k1 <- rbind(t1$k1, t2$k1)
+  k10 <- rbind(t$k10, t2$k10)
+  k50 <- rbind(t1$k50, t2$k50)
+  k100 <- rbind(t1$k100, t2$k100)
+  return(list(k1=k1,k10=k10,k50=k50,k100=k100))
+}
